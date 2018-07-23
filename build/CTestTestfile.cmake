c1c48f03e8b74832fad41d9791e99c308c864035
Carlos Sanoja <wuopcarlos@gmail.com>
2018-07-23 16:26:46 -0700

    init

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/robond/Documents/Localization_Project/src
+# Build directory: /home/robond/Documents/Localization_Project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
