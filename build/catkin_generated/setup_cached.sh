7b250b5679849a3a3dd72f8c3569d02e322482dd
Carlos Sanoja <wuopcarlos@gmail.com>
2018-07-23 17:29:09 -0700

    catkin_make

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,7 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/robond/Documents/Localization_Project/devel:$CMAKE_PREFIX_PATH"
 export PWD="/home/robond/Documents/Localization_Project/build"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/robond/Documents/Localization_Project/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
-export ROS_PACKAGE_PATH="/home/robond/Documents/Localization_Project/src:$ROS_PACKAGE_PATH"
---
+++ build/udacity_bot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/robond/Documents/Localization_Project/src/udacity_bot
+# Build directory: /home/robond/Documents/Localization_Project/build/udacity_bot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
